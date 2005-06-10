8a0ffa4abaec809460843eeebe067ea92d21587e
perk <perk@dd13fc19-a719-4ce7-b0cd-dbb51030e412>
2005-06-10 22:16:30 +0000

    Minor corrections needed after use of time_series/utility
    git-svn-id: https://cvs.pnl.gov/svn/MASS/mass1/trunk@154 dd13fc19-a719-4ce7-b0cd-dbb51030e412

--- gage_output.f90
+++ gage_output.f90
@@ -3,7 +3,7 @@
 !            Pacific Northwest National Laboratory
 !***************************************************************
 !
-! NAME:	gage_output
+! NAME: gage_output
 !
 ! VERSION and DATE: MASS1 v0.61 11/21/1997
 !
@@ -60,43 +60,43 @@ IF(time == time_begin )THEN
     CALL open_existing(filename(14), fileunit(14), fatal=.TRUE.)
 
     DO WHILE(.TRUE.)
-	count=count+1	
-	READ(fileunit(14),*, END=100)gage_link(count),gage_point(count)
-	END DO
+    count=count+1   
+    READ(fileunit(14),*, END=100)gage_link(count),gage_point(count)
+    END DO
 100     CLOSE(fileunit(14))
     IF (count .gt. 0) count = count - 1
-	num_gages=count
-	DO i=1,num_gages
-	count = 50 + i
+    num_gages=count
+    DO i=1,num_gages
+    count = 50 + i
         fname = ''
-	fname(1:2) = 'ts'
-	WRITE(string1,*)gage_link(i)
-	WRITE(string2,*)gage_point(i)
-	!READ(gage_point(i),*)string2
-	string1 = ADJUSTL(string1)
-	string2 = ADJUSTL(string2)
-	len1 = LEN_TRIM(string1)
-	len2 = LEN_TRIM(string2)
-	spot1 =3 + len1 - 1
-	fname(3:spot1) = string1(1:len1)
-	spot1 = spot1 + 1
-	spot2 = spot1 + len2 - 1
-	fname(spot1:spot2) = string2(1:len2)
-	spot1 = spot2 + 1
-	spot2 = spot1 + LEN_TRIM('.out')
-	fname(spot1:spot2) = '.out'
-	OPEN(count,file=fname)
-	!WRITE(count,1005)'date','time','water el','discharge','velocity','depth ', &
+    fname(1:2) = 'ts'
+    WRITE(string1,*)gage_link(i)
+    WRITE(string2,*)gage_point(i)
+    !READ(gage_point(i),*)string2
+    string1 = ADJUSTL(string1)
+    string2 = ADJUSTL(string2)
+    len1 = LEN_TRIM(string1)
+    len2 = LEN_TRIM(string2)
+    spot1 =3 + len1 - 1
+    fname(3:spot1) = string1(1:len1)
+    spot1 = spot1 + 1
+    spot2 = spot1 + len2 - 1
+    fname(spot1:spot2) = string2(1:len2)
+    spot1 = spot2 + 1
+    spot2 = spot1 + LEN_TRIM('.out')
+    fname(spot1:spot2) = '.out'
+    OPEN(count,file=fname)
+    !WRITE(count,1005)'date','time','water el','discharge','velocity','depth ', &
     ! 'conc ','temp ','thalweg el ','area ','top width ','hyd radius ','Froude Num ','frict slope ', &
-	! 'bed shear '
+    ! 'bed shear '
 !1005 FORMAT(2a7,20a12)
-	WRITE(count,1005)
+    WRITE(count,1005)
 1005 FORMAT('#',1x,'date',8x,'time',5x,'water elev',2x,'discharge',5x,'vel',4x,'depth', &
      7x,'conc',4x,'temp' ,2x,'%Sat',3x,'TDG P', &
-		4x,'thalweg el',2x,'area ',2x,'top width',2x,'hyd rad',2x,'Fr #',2x,'frict slope', &
-		2x,'bed shear')
+        4x,'thalweg el',2x,'area ',2x,'top width',2x,'hyd rad',2x,'Fr #',2x,'frict slope', &
+        2x,'bed shear')
 
-	END DO
+    END DO
 
     CALL hydro_output_setup()
 
@@ -117,8 +117,8 @@ CALL decimal_to_date(accum_time)
 
 !!$WRITE(count,1010)date_string,time_string,y(link,point),q(link,point),vel(link,point),depth, &
 !!$     species(1)%conc(link,point),species(2)%conc(link,point), tdg_sat, tdg_press, &
-!!$		thalweg(link,point),area(link,point),top_width(link,point),hyd_radius(link,point), &
-!!$		froude_num(link,point),friction_slope(link,point),bed_shear(link,point)
+!!$     thalweg(link,point),area(link,point),top_width(link,point),hyd_radius(link,point), &
+!!$     froude_num(link,point),friction_slope(link,point),bed_shear(link,point)
 WRITE(count,1010)date_string,time_string,&
      &accum_var%y%sum(link,point),accum_var%q%sum(link,point),&
      &accum_var%vel%sum(link,point),depth, &
@@ -135,8 +135,8 @@ WRITE(count,1010)date_string,time_string,&
 
 
 1010 FORMAT(a10,2x,a8,2x,f8.2,2x,f12.2,2x,f6.2,2x,f7.2,2x,f10.2,2x,f6.2,2x,f6.2,2x,f6.1,2x, &
-			f8.2,2x,es10.2,2x, &
-			f8.2,2x,f6.2,f6.2,es10.2,2x,es10.2)
+            f8.2,2x,es10.2,2x, &
+            f8.2,2x,f6.2,f6.2,es10.2,2x,es10.2)
 
 !WRITE(count,1000)time/time_mult,q(link,point),y(link,point),depth,vel(link,point),c(link,point),temp(link,point)
 
@@ -148,10 +148,10 @@ IF (time > time_begin) CALL hydro_output(date_string,time_string)
 
 IF(time >= time_end)THEN
 
-	DO i=1,num_gages
-	count = 50 + i
-	CLOSE(count)
-	END DO
+    DO i=1,num_gages
+    count = 50 + i
+    CLOSE(count)
+    END DO
 
     CALL hydro_output_done()
 ENDIF
