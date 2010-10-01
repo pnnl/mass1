e66f6e332b724677ce46fd194e7e0b345f0831ad
d3g096 <d3g096@dd13fc19-a719-4ce7-b0cd-dbb51030e412>
2010-10-01 19:24:20 +0000

    added
    git-svn-id: https://cvs.pnl.gov/svn/MASS/mass1/trunk@188 dd13fc19-a719-4ce7-b0cd-dbb51030e412

---
+++ test/temperature/pond/runit.sh
@@ -0,0 +1,30 @@
+#! /bin/sh
+# -------------------------------------------------------------
+# file: runit.sh
+# -------------------------------------------------------------
+# -------------------------------------------------------------
+# Battelle Memorial Institute
+# Pacific Northwest Laboratory
+# -------------------------------------------------------------
+# -------------------------------------------------------------
+# Created December 11, 1998 by William A. Perkins
+# Last Change: Thu Sep 30 13:46:06 2010 by William A. Perkins <d3g096@PE10900.pnl.gov>
+# -------------------------------------------------------------
+# $Id$
+
+set -x
+set -e
+
+                                # to trap floating point errors on SGI
+
+TRAP_FPE='INVALID=ABORT(1);UNDERFL=ZERO;OVERFL=ABORT(1);INT_OVERFL=ABORT(1);DIVZERO=ABORT(1);DEBUG'
+export TRAP_FPE
+
+model=${MODEL-../../../mass1}
+
+$model
+
+gnuplot temp.gp > temp.eps
+gnuplot temp.1992.gp > temp.1992.eps
+gnuplot temp.summer.1992.gp > temp.summer.1992.eps
+
