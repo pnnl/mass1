c8c9539d1849afa1b546a041e7baecf5b7bd07bc
d3g096 <d3g096@dd13fc19-a719-4ce7-b0cd-dbb51030e412>
2010-12-01 20:11:39 +0000

    new type to deal with BC's that uses the time_series code
    git-svn-id: https://cvs.pnl.gov/svn/MASS/mass1/trunk@198 dd13fc19-a719-4ce7-b0cd-dbb51030e412

---
+++ bctable.f90
@@ -0,0 +1,158 @@
+! ----------------------------------------------------------------
+! file: bctable.f90
+! ----------------------------------------------------------------
+! ----------------------------------------------------------------
+! Battelle Memorial Institute
+! Pacific Northwest Laboratory
+! ----------------------------------------------------------------
+! ----------------------------------------------------------------
+! Created November 30, 2010 by William A. Perkins
+! Last Change: Tue Nov 30 14:06:36 2010 by William A. Perkins <d3g096@PE10900.pnl.gov>
+! ----------------------------------------------------------------
+  
+! RCS ID: $Id$ Battelle PNL
+
+! ----------------------------------------------------------------
+! MODULE bctable
+! ----------------------------------------------------------------
+MODULE bctable
+
+  USE utility
+  USE time_series
+
+  IMPLICIT NONE
+
+  CHARACTER (LEN=80), PRIVATE, SAVE :: rcsid = "$Id$"
+
+  ! ----------------------------------------------------------------
+  ! TYPE bc_table_entry
+  ! ----------------------------------------------------------------
+  TYPE bc_table_entry
+     INTEGER :: id              ! identifier used in bc table file
+     TYPE (time_series_rec), POINTER :: bc ! the time series
+  END TYPE bc_table_entry
+
+  ! ----------------------------------------------------------------
+  ! TYPE bc_table
+  ! ----------------------------------------------------------------
+  TYPE bc_table
+     INTEGER :: nbc
+     INTEGER :: maxid
+     INTEGER :: nfld
+     INTEGER, ALLOCATABLE :: idlookup(:)
+     TYPE (bc_table_entry), ALLOCATABLE :: bc(:)
+  END TYPE bc_table
+
+
+  TYPE (bc_table), PUBLIC, POINTER :: linkbc
+  TYPE (bc_table), PUBLIC, POINTER :: hydrobc
+  TYPE (bc_table), PUBLIC, POINTER :: tempbc
+  TYPE (bc_table), PUBLIC, POINTER :: transbc
+  TYPE (bc_table), PUBLIC, POINTER :: latflowbc
+
+CONTAINS
+
+
+  ! ----------------------------------------------------------------
+  ! TYPE (BC_TABLE) FUNCTION bc_table_read
+  ! ----------------------------------------------------------------
+  TYPE (BC_TABLE) FUNCTION bc_table_read(filename, nfields) RESULT (tbl)
+
+    IMPLICIT NONE
+
+    CHARACTER(LEN=*), INTENT(IN) :: filename
+    INTEGER, INTENT(IN) :: nfields
+
+    POINTER tbl
+
+    INTEGER, PARAMETER :: iounit = 53
+
+    INTEGER :: count, maxid, i
+    INTEGER :: bcid
+    CHARACTER(LEN=1024) :: bcfile
+
+    CALL open_existing(filename, iounit, fatal=.TRUE.)
+
+    ! count the number of entries, and find the maximum bc id
+
+    count = 0
+    maxid = -1
+    
+    DO WHILE(.TRUE.)
+       READ(iounit, *, END=100, ERR=1000) bcid, bcfile
+       count = count + 1
+       if (bcid .GT. maxid) maxid = bcid
+    END DO
+100 CONTINUE
+
+    ALLOCATE(tbl)
+    tbl%nbc = count
+    tbl%maxid = maxid
+    tbl%nfld = nfields
+    ALLOCATE(tbl%idlookup(maxid))
+    tbl%idlookup = 0
+    ALLOCATE(tbl%bc(count))
+    
+    REWIND(iounit)
+    i = 1
+    DO WHILE(.TRUE.)
+       READ(iounit, *, END=101, ERR=1000) bcid, bcfile
+       tbl%idlookup(bcid) = i
+       tbl%bc(i)%id = bcid
+       tbl%bc(i)%bc => time_series_read(bcfile, nfields)
+       i = i + 1
+    END DO
+101 CONTINUE
+
+    CLOSE(iounit)
+
+    RETURN
+1000 CONTINUE 
+
+    CALL error_message("Error reading BC table" // TRIM(filename), .TRUE.)
+  END FUNCTION bc_table_read
+
+
+  ! ----------------------------------------------------------------
+  ! SUBROUTINE bc_table_interpolate
+  ! ----------------------------------------------------------------
+  SUBROUTINE bc_table_interpolate(tbl, id, datetime)
+
+    IMPLICIT NONE
+
+    TYPE (bc_table), INTENT(IN) :: tbl
+    INTEGER, INTENT(IN) :: id
+    DOUBLE PRECISION, INTENT(IN) :: datetime
+
+    INTEGER :: idx
+
+    idx = tbl%idlookup(id)
+
+    IF (idx .LE. 0) THEN
+       CALL error_message("Nonexistent BC table index", .TRUE.)
+    END IF
+    
+    CALL time_series_interp(tbl%bc(idx)%bc, datetime)
+
+  END SUBROUTINE bc_table_interpolate
+
+  ! ----------------------------------------------------------------
+  ! DOUBLE PRECISION FUNCTION bc_table_current
+  ! ----------------------------------------------------------------
+  DOUBLE PRECISION FUNCTION bc_table_current(tbl, id, fld)
+
+    IMPLICIT NONE
+
+    TYPE (bc_table), INTENT(IN) :: tbl
+    INTEGER, INTENT(IN) :: id
+    INTEGER, INTENT(IN) :: fld
+
+    INTEGER idx
+
+    idx = tbl%idlookup(id)
+    bc_table_current = tbl%bc(idx)%bc%current(fld)
+  END FUNCTION bc_table_current
+
+
+
+END MODULE bctable
