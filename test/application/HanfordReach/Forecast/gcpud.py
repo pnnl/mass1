8827c93e690ffcedecdeec05c3cb5793b318163e
d3g096 <d3g096@dd13fc19-a719-4ce7-b0cd-dbb51030e412>
2011-03-10 00:21:19 +0000

    added
    git-svn-id: https://cvs.pnl.gov/svn/MASS/mass1/trunk@283 dd13fc19-a719-4ce7-b0cd-dbb51030e412

---
+++ test/application/HanfordReach/Forecast/gcpud.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python2
+# -*- mode: python; py-which-shell: "python2";-*-
+# -------------------------------------------------------------
+# file: gcpud.py
+# -------------------------------------------------------------
+# -------------------------------------------------------------
+# Battelle Memorial Institute
+# Pacific Northwest Laboratory
+# -------------------------------------------------------------
+# -------------------------------------------------------------
+# Created March  9, 2011 by William A. Perkins
+# Last Change: Wed Mar  9 15:18:45 2011 by William A. Perkins <d3g096@bearflag.pnl.gov>
+# -------------------------------------------------------------
+
+# RCS ID: $Id$
+
+import getopt, sys, os
+import urllib
+import urllib2
+from datetime import *
+from time import *
+
+thefmt = "%m-%d-%Y %H:%M:%S"
+
+# -------------------------------------------------------------
+# download_prdq_recent
+#
+# Gets PRD discharge directly from GCPUD.  Usually current through
+# midnight the previous day.
+# -------------------------------------------------------------
+def download_prdq_recent(now, outname):
+    urlbase = "http://www.gcpud.org/data/water/fixed/%Y/csvform/%m%d%y.csv"
+    m = now.month
+    d = now.day
+    y = now.year
+
+    offset = timedelta(minutes=30)
+
+    start = datetime(y, m, d, 0, 0)
+    start -= timedelta(days=10)
+
+    outf = open(outname, "w")
+    outf.write("# Priest Rapids Discharge, retrieved %s from www.gcpud.org\n" %
+           (datetime.today().strftime("%m/%d/%Y %H:%M:%S %Z%z")))
+    
+
+    while (start <= now):
+        url = start.strftime(urlbase)
+        sys.stderr.write("Trying URL: %s\n" % (url))
+        start += timedelta(1)
+        try:
+            f = urllib2.urlopen(url)
+        except:
+            sys.stderr.write("unable to get URL: %s\n" % (url))
+            continue
+
+        thedate = None
+        lnum = 0
+        for l in f:
+            l.rstrip()
+            lnum += 1
+            fld = l.split(",")
+            if (fld[1].find("/") > 0):
+                lt = strptime(fld[1] + " " + fld[2], "%m/%d/%Y %H%M")
+                d = datetime(lt.tm_year, lt.tm_mon, lt.tm_mday,
+                             hour=lt.tm_hour, minute=lt.tm_min, second=lt.tm_sec)
+                q = float(fld[19])*1000.0
+                if (d <= now):
+                    d -= offset
+                    outf.write("%s %9.1f /\n" % (d.strftime(thefmt), q))
+        f.close()
+    outf.close()
+    return
+
+
+# -------------------------------------------------------------
+# variable initialization
+# -------------------------------------------------------------
+program = os.path.basename(sys.argv[0])
+usage = "usage: " + program
+
+# -------------------------------------------------------------
+# handle command line
+# -------------------------------------------------------------
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "h?")
+except getopt.GetoptError:
+    sys.stderr.write(usage + "\n")
+    sys.exit(2)
+
+for o, a in opts:
+    if (o == "-h" or o == "-?"):
+        sys.stderr.write(usage + "\n")
+        sys.exit(0)
+
+# -------------------------------------------------------------
+# main program
+# -------------------------------------------------------------
+
+now = datetime.today() - timedelta(30)
+download_prdq_recent(now, "PRD-Qtotal.dat")
---
+++ test/application/HanfordReach/Forecast/plotall.gp
@@ -0,0 +1,17 @@
+set terminal png font "/usr/lib/jvm/java-1.4.2-ibm-1.4.2.1.x86_64/jre/lib/fonts/LucidaSansRegular.ttf,11" size 800,600
+set xdata time
+set timefmt '%m-%d-%Y %H:%M:%S'
+set xrange ["@PSTART@": "@PEND@"] noreverse
+
+thefile = "results.txt"
+now = "@NOW@"
+
+set arrow 1 from first now, graph 0 to first now, graph 1 nohead lt 7
+set grid
+set timestamp
+set format x "%d%b%y\n%H:%M"
+
+quad = 1
+
+load 'plot1quad.gp'
+
