5ff6a0b07904dc836dffe22cec9aaf69b7539cfe
d3g096 <d3g096@dd13fc19-a719-4ce7-b0cd-dbb51030e412>
2011-03-23 18:35:21 +0000

    copy results and graphs here, not in runmass1.py
    git-svn-id: https://cvs.pnl.gov/svn/MASS/mass1/trunk@294 dd13fc19-a719-4ce7-b0cd-dbb51030e412

--- test/application/HanfordReach/Forecast/run.sh
+++ test/application/HanfordReach/Forecast/run.sh
@@ -8,12 +8,19 @@
 # -------------------------------------------------------------
 # -------------------------------------------------------------
 # Created January 27, 2011 by William A. Perkins
-# Last Change: Mon Feb 21 10:28:53 2011 by William A. Perkins <d3g096@PE10900.pnl.gov>
+# Last Change: Wed Mar 23 11:19:45 2011 by William A. Perkins <d3g096@bearflag.pnl.gov>
 # -------------------------------------------------------------
 set -ue
 
-# cd /home/d3g096/Projects/GrantPUD/HanfordReachForecast/test/Forecast
+PATH=/usr/local/bin:$PATH
+export path
+
+cd /home/d3g096/Projects/GrantPUD/HanfordReachForecast/test/Forecast
+exec 2>&1
 exec > run.log
 MODEL="../../../../mass1"
 export MODEL
 /usr/local/python2.5/bin/python ./runmass1.py 2>&1
+cp results.txt /projects/hanford_forecast/current/mass1-current.csv
+cp q???.png e???.png /projects/hanford_forecast/current
+
