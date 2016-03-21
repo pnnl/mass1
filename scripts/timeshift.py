18d3f139ab9c16c5d670bacc7ffe79a9be4a146d
d3g096@PNL.GOV <d3g096@PNL.GOV@dd13fc19-a719-4ce7-b0cd-dbb51030e412>
2016-03-21 19:59:32 +0000

    mass1: add a script to time shift MASS1/2 BC files
    git-svn-id: https://cvs.pnl.gov/svn/MASS/mass1/trunk@1278 dd13fc19-a719-4ce7-b0cd-dbb51030e412

---
+++ scripts/timeshift.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python2
+# -*- mode: python; py-which-shell: "python2";-*-
+# -------------------------------------------------------------
+# file: timeshift.py
+# -------------------------------------------------------------
+# -------------------------------------------------------------
+# Battelle Memorial Institute
+# Pacific Northwest Laboratory
+# -------------------------------------------------------------
+# -------------------------------------------------------------
+# Created March  9, 2016 by William A. Perkins
+# Last Change: 2016-03-09 07:21:24 d3g096
+# -------------------------------------------------------------
+
+# RCS ID: $Id$
+
+import sys, os
+from optparse import OptionParser
+import re
+from datetime import *
+import fileinput
+
+# -------------------------------------------------------------
+# variable initialization
+# -------------------------------------------------------------
+program = os.path.basename(sys.argv[0])
+usage = "usage: " + program
+
+fmt = "%m-%d-%Y %H:%M:%S"
+
+# -------------------------------------------------------------
+# handle command line
+# -------------------------------------------------------------
+usage = "Usage: %prog [options] [file]"
+parser = OptionParser(usage=usage)
+
+parser.add_option("-v", "--verbose",
+                  dest="verbose", action="store_true", default=False,
+                  help="show what's going on")
+
+parser.add_option("-o", "--output", type="string",
+                  dest="output", action="store")
+
+parser.add_option("-H", "--hour-offset", type="int", default=0,
+                  dest="hroffset", action="store")
+
+(options, args) = parser.parse_args()
+
+doverbose = options.verbose
+hroffset = options.hroffset
+
+theoffset = timedelta(hours=int(hroffset))
+
+if options.output:
+    output = open(options.output, mode='w')
+else:
+    output = sys.stdout
+
+# -------------------------------------------------------------
+# main program
+# -------------------------------------------------------------
+
+rdatetime = re.compile(r'^\s*(\d\d)-(\d\d)-(\d\d\d\d)\s+(\d\d):(\d\d):(\d\d)(.*)$')
+
+if len(args) > 0:
+    inp = fileinput.input(args[0])
+else:
+    inp = fileinput.input("-") 
+
+for line in inp:
+    l = line.rstrip()
+    m = rdatetime.match(l)
+    if (rdatetime.match(l)):
+        s = rdatetime.split(l)      # need to ignore s[0]
+        pdatetime = datetime(month=int(s[1]), day=int(s[2]), year=int(s[3]),
+                             hour=int(s[4]), minute=int(s[5]), second=int(s[6]));
+        newtime = pdatetime + theoffset
+        output.write("%s %s\n" % (newtime.strftime(fmt), s[7]))
+        
+    else:
+        output.write("%s\n" % (l))
