05413201db722d750701715124c04f32131a4443
d3g096 <d3g096>
2016-05-17 14:46:15 +0000

    minor output format changes

--- CHARIMAthalweg.pl
+++ CHARIMAthalweg.pl
@@ -9,7 +9,7 @@
 # -------------------------------------------------------------
 # -------------------------------------------------------------
 # Created November 27, 1996 by William A. Perkins
-# Last Change: Mon Dec  2 10:21:03 1996 by William A. Perkins <perk@doofus.pnl.gov>
+# Last Change: Mon Feb 21 08:03:55 2011 by William A. Perkins <d3g096@PE10900.pnl.gov>
 # -------------------------------------------------------------
 
 # RCS ID: $Id$
@@ -26,7 +26,7 @@ use XSection;
 # -------------------------------------------------------------
 while (($xsection = CHARIMASection::read_section(STDIN)) != 0) {
   ($x, $z) = $xsection->thalweg();
-  printf("%5d %10.2f %10.2f %10.2f\n", $xsection->{ID}, 
+  printf("%5d %10.3f %10.2f %10.3f\n", $xsection->{ID}, 
          $xsection->{rivermile}, $x, $z);
   # printf("%s (%d): %s\n", $xsection->{river}, $xsection->points(), 
   #        $xsection->{comment});
