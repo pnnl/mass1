242c0a5f7a90d588008bd708387fd8795fc1f583
William Perkins <william.perkins@pnnl.gov>
2017-07-05 10:33:11 -0700

    Add sweep coefficient code

--- linear_link_module.f90
+++ linear_link_module.f90
@@ -7,7 +7,7 @@
 ! ----------------------------------------------------------------
 ! ----------------------------------------------------------------
 ! Created June 28, 2017 by William A. Perkins
-! Last Change: 2017-07-03 13:17:43 d3g096
+! Last Change: 2017-07-03 15:04:52 d3g096
 ! ----------------------------------------------------------------
 ! ----------------------------------------------------------------
 ! MODULE linear_link_module
@@ -39,8 +39,12 @@ MODULE linear_link_module
      CLASS (xsection_ptr), POINTER :: xsection
      TYPE (xsection_prop) :: xsprop
      TYPE (point_hydro_state) :: hstate
-     TYPE (point_sweep_coeff) :: coeff
+     TYPE (point_sweep_coeff) :: sweep
   END type point_t
+
+  TYPE :: coeff
+     DOUBLE PRECISION :: a, b, c, d, g
+  END type coeff
 
   TYPE, PUBLIC, EXTENDS(link_t) :: linear_link_t
      INTEGER :: npoints
@@ -55,6 +59,7 @@ MODULE linear_link_module
      PROCEDURE :: y_down => linear_link_y_down
      PROCEDURE :: c_up => linear_link_c_up
      PROCEDURE :: c_down => linear_link_c_down
+     PROCEDURE :: coeff => linear_link_coeff
      PROCEDURE :: forward_sweep => linear_link_forward
      PROCEDURE :: backward_sweep => linear_link_backward
      PROCEDURE :: destroy => linear_link_destroy
@@ -82,7 +87,9 @@ CONTAINS
   DOUBLE PRECISION FUNCTION linear_link_q_up(this)
     IMPLICIT NONE
     CLASS (linear_link_t), INTENT(IN) :: this
-    linear_link_q_up = 0.0
+    INTEGER :: n
+    n = 1
+    linear_link_q_up = this%pt(n)%hstate%q
   END FUNCTION linear_link_q_up
 
 
@@ -92,7 +99,9 @@ CONTAINS
   DOUBLE PRECISION FUNCTION linear_link_q_down(this)
     IMPLICIT NONE
     CLASS (linear_link_t), INTENT(IN) :: this
-    linear_link_q_down = 0.0
+    INTEGER :: n
+    n = this%npoints
+    linear_link_q_down = this%pt(n)%hstate%q
   END FUNCTION linear_link_q_down
 
 
@@ -102,7 +111,9 @@ CONTAINS
   DOUBLE PRECISION FUNCTION linear_link_y_up(this)
     IMPLICIT NONE
     CLASS (linear_link_t), INTENT(IN) :: this
-    linear_link_y_up = 0.0
+    INTEGER :: n
+    n = 1
+    linear_link_y_up = this%pt(n)%hstate%y
   END FUNCTION linear_link_y_up
 
 
@@ -112,7 +123,9 @@ CONTAINS
   DOUBLE PRECISION FUNCTION linear_link_y_down(this)
     IMPLICIT NONE
     CLASS (linear_link_t), INTENT(IN) :: this
-    linear_link_y_down = 0.0
+    INTEGER :: n
+    n = this%npoints
+    linear_link_y_down = this%pt(n)%hstate%y
   END FUNCTION linear_link_y_down
 
 
@@ -138,6 +151,21 @@ CONTAINS
   END FUNCTION linear_link_c_down
 
   ! ----------------------------------------------------------------
+  ! SUBROUTINE linear_link_coeff
+  ! ----------------------------------------------------------------
+  SUBROUTINE linear_link_coeff(this, pt1, pt2, c, cp)
+
+    IMPLICIT NONE
+    CLASS (linear_link_t), INTENT(IN) :: this
+    TYPE (point_t), INTENT(IN) :: pt1, pt2
+    TYPE (coeff), INTENT(OUT) :: c, cp
+
+    CALL error_message("This should not happen: linear_link_coeff should be overridden", &
+         &fatal=.TRUE.)
+  END SUBROUTINE linear_link_coeff
+
+
+  ! ----------------------------------------------------------------
   ! SUBROUTINE linear_link_forward
   ! ----------------------------------------------------------------
   SUBROUTINE linear_link_forward(this)
@@ -145,14 +173,39 @@ CONTAINS
     IMPLICIT NONE
     CLASS (linear_link_t), INTENT(INOUT) :: this
 
-
+    INTEGER :: point
+    DOUBLE PRECISION :: bcval, denom
+    TYPE (coeff) :: c, cp
+    
+    point = 1
     IF (ASSOCIATED(this%ucon%p)) THEN
-    ELSEIF (ASSOCIATED(this%usbc%p)) THEN
-    ELSE 
-       CALL error_message("This should not happen in linear_link_forward", &
-            &fatal=.TRUE.)
+       this%pt(point)%sweep%e = this%ucon%p%coeff_e()
+       this%pt(point)%sweep%f = this%ucon%p%coeff_f()
+    ELSE
+       IF (ASSOCIATED(this%usbc%p)) THEN
+          bcval = this%usbc%p%current_value
+       ELSE 
+          bcval = 0.0
+       END IF
+       this%pt(point)%sweep%e = 0.0
+       this%pt(point)%sweep%f = bcval - this%pt(point)%hstate%q
     END IF
 
+    DO point = 1, this%npoints - 1
+       CALL this%coeff(this%pt(point), this%pt(point + 1), c, cp)
+       denom = (c%c*cp%d - cp%c*c%d)
+       this%pt(point)%sweep%l = (c%a*cp%d - cp%a*c%d)/denom
+       this%pt(point)%sweep%m = (c%b*cp%d - cp%b*c%d)/denom
+       this%pt(point)%sweep%n = (c%d*cp%g - cp%d*c%g)/denom
+
+       denom = c%b - this%pt(point)%sweep%m*(c%c + c%d*this%pt(point)%sweep%e)
+       this%pt(point+1)%sweep%e = &
+            &(this%pt(point)%sweep%l*(c%c + c%d*this%pt(point)%sweep%e) - c%a)/denom
+       this%pt(point+1)%sweep%f = &
+            &(this%pt(point)%sweep%n*(c%c + c%d*this%pt(point)%sweep%e) + &
+            &c%d*this%pt(point)%sweep%f + c%g)/denom
+
+    END DO
 
 
   END SUBROUTINE linear_link_forward
@@ -165,16 +218,34 @@ CONTAINS
 
     IMPLICIT NONE
     CLASS (linear_link_t), INTENT(INOUT) :: this
+    DOUBLE PRECISION :: bcval, dy, dq
+    INTEGER :: point
+
+    point = this%npoints
+    
+    IF (ASSOCIATED(this%dcon%p)) THEN
+
+       dy = this%dcon%p%elev() - this%pt(point)%hstate%y
+       dq = this%pt(point)%sweep%e*dy + this%pt(point)%sweep%f
+       this%pt(point)%hstate%y = this%pt(point)%hstate%y + dy
+       this%pt(point)%hstate%q = this%pt(point)%hstate%q + dq
+
+    ELSE IF (ASSOCIATED(this%dsbc%p)) THEN
 
 
-    IF (ASSOCIATED(this%ucon%p)) THEN
-    ELSEIF (ASSOCIATED(this%usbc%p)) THEN
     ELSE 
        CALL error_message("This should not happen in linear_link_backward", &
             &fatal=.TRUE.)
     END IF
 
+    DO point = this%npoints - 1, 1, -1
+       dy = this%pt(point)%sweep%l*dy + this%pt(point)%sweep%m*dq + this%pt(point)%sweep%n
+       dq = this%pt(point)%sweep%e*dy + this%pt(point)%sweep%f
 
+       this%pt(point)%hstate%y = this%pt(point)%hstate%y + dy
+       this%pt(point)%hstate%q = this%pt(point)%hstate%q + dq
+       
+    END DO
 
   END SUBROUTINE linear_link_backward
 
