a7eea025ec5accba55b47fd82ebfd0d38e40778d
William Perkins <william.perkins@pnnl.gov>
2018-01-08 12:52:34 -0800

    new MASS1: Compute fluvial coefficients using the old routine

--- fluvial_link_module.f90
+++ fluvial_link_module.f90
@@ -7,7 +7,7 @@
 ! ----------------------------------------------------------------
 ! ----------------------------------------------------------------
 ! Created July  3, 2017 by William A. Perkins
-! Last Change: 2018-01-08 11:13:53 d3g096
+! Last Change: 2018-01-08 12:17:28 d3g096
 ! ----------------------------------------------------------------
 ! ----------------------------------------------------------------
 ! MODULE fluvial_link_module
@@ -78,11 +78,14 @@ CONTAINS
   ! SUBROUTINE fluvial_link_coeff
   ! ----------------------------------------------------------------
   SUBROUTINE fluvial_link_coeff(this, dt, pt1, pt2, cf)
+    USE fluvial_coeffs
     IMPLICIT NONE
     CLASS (fluvial_link), INTENT(IN) :: this
     DOUBLE PRECISION, INTENT(IN) :: dt
     TYPE (point_t), INTENT(IN) :: pt1, pt2
     TYPE (coeff), INTENT(OUT) :: cf
+
+    TYPE (fluvial_state) :: s1, s2
 
     DOUBLE PRECISION :: y1, y2, d1, d2, q1, q2, a1, a2, b1, b2, k1, k2, ky1, ky2
     DOUBLE PRECISION :: fr1, fr2
@@ -96,70 +99,25 @@ CONTAINS
     CALL pt2%assign(y2, d2, q2, a2, b2, k2, ky2, fr2)
 
     dx = ABS(pt1%x - pt2%x)
-
-    cf%a = b2/2.0/dt
-    cf%b = theta/dx
-    cf%c = -b1/2.0/dt
-    cf%d = cf%b
-    cf%g = (q1 - q2)/dx + theta*this%latq + (1 - theta)*this%latqold
-
-    !--------------------------------------------------------
-    ! momemtum equation coefficients
-
-    sigma = 1.0
-
-    ! check for trans-critical flow and adjust inertial terms according
-    ! to the LPI method of Fread, Jin, and Lewis, 1996
-
-    IF (this%lpiexp .GE. 1.0) THEN
-       fravg = 0.5*(fr1 + fr2)
-       IF (fravg .LE. 1.0) THEN
-          sigma = 1.0 - fravg**this%lpiexp
-       ELSE 
-          sigma = 0.0
-       END IF
-    END IF
-
-    ! if the depth is small, completely turn off the inertial terms and
-    ! change the friction weighting
-    IF ( (d1 .LT. depth_threshold) .OR. (d2 .LT. depth_threshold)) THEN
-       sigma = 0.0
-       beta = 1.0
-    ELSE 
-       beta = 0.5
-    END IF
-
-
-    cf%a = alpha*theta*sigma*(-q2*b2*(q2-q1)/dx/a2**2           &
-         + b2*(q2/a2**2)*(a2-a1)*(q2/a2+q1/a1)/dx/2.0   &
-         - b2*(q2/a2 + q1/a1)**2/dx/4.0)                            &
-         + gr*theta*(b2*(y2-y1)/dx/2.0 + (a2+a1)/2.0/dx   &
-         + b2*(beta*q1*abs(q1)/k1**2 + (1.0 - beta)*q2    &
-         *abs(q2)/k2**2)/2.0                                                              &
-         -(1.0 - beta)*q2*abs(q2)*(ky2/k2**3)*(a2+a1))
-
-    cf%c = alpha*theta*sigma*(b1*q1*(q2-q1)/dx/a1**2       &
-         - b1*(q1/a1**2)*(a2-a1)*(q2/a2+q1/a1)/dx/2.0  &
-         - b1*(q2/a2+q1/a1)**2/dx/4.0)                             &
-         + gr*theta*(-b1*(y2-y1)/dx/2.0 + (a2+a1)/2.0/dx  &
-         - b1*(beta*q1*abs(q1)/k1**2+(1.0-beta)*q2                &
-         *abs(q2)/k2**2)/2.0                                                              &
-         + beta*q1*abs(q1)*(ky1/k1**3)*(a2+a1))
-
-    cf%b = sigma*1.0/2.0/dt+alpha*theta*sigma*(2.0*q2/a2+q1*(1.0/a1-1.0/a2) &
-         -0.5*(1.0-a1/a2)*(q2/a2+q1/a1))/dx                                              &
-         +gr*theta*(1.0- beta)*abs(q2)*(a2+a1)/k2**2
-
-    cf%d = -sigma*1.0/2.0/dt-alpha*theta*sigma*(-2.0*q1/a1+q2*(1.0/a1-1.0/a2) &
-         -0.5*(a2/a1-1.0)*(q2/a2 + q1/a1))/dx                                      &
-         -gr*theta*beta*(a2+a1)*abs(q1)/k1**2
-
-    gp1 = -alpha*sigma*(q2/a2+q1/a1)*(q2-q1)/dx                      
-    gp2 = alpha*sigma*((q2/a2+q1/a1)**2)*(a2-a1)/dx/4.0
-    gp3 = -gr*(a2+a1)*(y2-y1)/2.0/dx
-    gp4 = -gr*(a2+a1)*(beta*q1*abs(q1)/k1**2+(1.0-beta)*q2  &
-         *abs(q2)/k2**2)/2.0
-    cf%g = gp1+gp2+gp3+gp4
+    s1%y = y1
+    s1%d = d1
+    s1%q = q1
+    s1%a = a1
+    s1%b = b1
+    s1%k = k1
+    s1%ky  = ky1
+    s1%fr = fr1
+    s2%y = y2
+    s2%d = d2
+    s2%q = q2
+    s2%a = a2
+    s2%b = b2
+    s2%k = k2
+    s2%ky  = ky2
+    s2%fr = fr2
+    
+    CALL fluvial_coeff(s1, s2, cf, dx, dt, gr, &
+         &this%latqold, this%latq, this%lpiexp, depth_threshold)
 
   END SUBROUTINE fluvial_link_coeff
 
