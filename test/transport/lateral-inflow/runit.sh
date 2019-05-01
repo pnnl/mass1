83d4bde85e99bb594ff1a01318e4dffd8513f3e7
William Perkins <william.perkins@pnnl.gov>
2019-05-01 13:17:54 -0700

    Add transport/lateral-inflow to CMake "runtests" target

--- test/transport/lateral-inflow/runit.sh
+++ test/transport/lateral-inflow/runit.sh
@@ -8,7 +8,7 @@
 # -------------------------------------------------------------
 # -------------------------------------------------------------
 # Created July  2, 1999 by William A. Perkins
-# Last Change: 2019-03-29 12:32:33 d3g096
+# Last Change: 2019-05-01 13:08:24 d3g096
 # -------------------------------------------------------------
 # $Id$
 
@@ -25,36 +25,39 @@ model=${MODEL-../../../build/mass1_new}
 rm -f mass1.cfg
 ln -f -s mass1-inflow.cfg mass1.cfg
 $model
+cp profile1.out profile1.inflow.out
 
 (echo \
     set terminal postscript portrait color solid \"Helvetica\" 14\; \
     set title \"Transport with Lateral Inflow\"\; \
+    pfile = \"profile1.inflow.out\"\; \
     load \"plot.gp\"\; ) | \
         gnuplot > plot-inflow.eps
 
-cp profile1.out profile1.inflow.out
 rm -f mass1.cfg
 ln -f -s mass1-outflow.cfg mass1.cfg
 $model
+cp profile1.out profile1.outflow.out
 rm -f mass1.cfg
 
 (echo \
     set terminal postscript portrait color solid \"Helvetica\" 14\; \
     set title \"Transport with Lateral Outflow\"\; \
+    pfile = \"profile1.outflow.out\"\; \
     load \"plot.gp\"\; ) | \
         gnuplot > plot-outflow.eps
 
-cp profile1.out profile1.outflow.out
 
 rm -f mass1.cfg
 ln -f -s mass1-vary.cfg mass1.cfg
 $model
+cp profile1.out profile1.vary.out
 rm -f mass1.cfg
 
 (echo \
     set terminal postscript portrait color solid \"Helvetica\" 14\; \
     set title \"Transport with Varying Lateral Inflow/Outflow\"\; \
+    pfile = \"profile1.vary.out\"\; \
     load \"plot.gp\"\; ) | \
         gnuplot > plot-vary.eps
 
-cp profile1.out profile1.vary.out
